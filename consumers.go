1f8302756b1c941623887f058d33bb643391b6b2
Helen Bailey <hakbailey@gmail.com>
2018-10-04 15:14:39 -0400

    Add JSON consumer and CLI flag for JSON output

--- consumers.go
+++ consumers.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+
 	"github.com/olivere/elastic"
 )
 
@@ -18,6 +21,21 @@ func (es *ESConsumer) Consume(recs <-chan Record, done chan<- bool) {
 	for r := range recs {
 		d := elastic.NewBulkIndexRequest().Index(es.Index).Type(es.RType).Doc(r)
 		es.p.Add(d)
+	}
+	done <- true
+}
+
+type JSONConsumer struct {
+	Output string
+}
+
+func (js *JSONConsumer) Consume(recs <-chan Record, done chan<- bool) {
+	for r := range recs {
+		b, err := json.MarshalIndent(r, "", "    ")
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println(string(b))
 	}
 	done <- true
 }
